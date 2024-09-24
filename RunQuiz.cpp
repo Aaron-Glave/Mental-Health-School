58ed7505e7ea29f96ce3184fe911c9461b626d71
Aaron-Glave <aaronglavenowbelivesingod@gmail.com>
2024-09-24 12:54:55 -0400

    Update RunQuiz.cpp

--- RunQuiz.cpp
+++ RunQuiz.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 
-int main(int argc, char *argv[]) {
+int main1(int argc, char *argv[]) {
 	int num_right = 0;
 	int num_questions = 5;
 	Question Colors_q_I_made = Question("What affects your sight?", 4, 3,
@@ -24,10 +24,10 @@ int main(int argc, char *argv[]) {
 		"Artificial intelligence",
 		"Staying the same");
 	Question story = Question("Describe at least TWO methods that might be used to study brain 'machinery' in neuropsychology patients", 1, 1,
-		"There are several methods that might be used to understand brain ‘machinery’ and understand certain functions.\n"
+		"There are several methods that might be used to understand brain â€˜machineryâ€™ and understand certain functions.\n"
 		"Neuropsychology patients may be used as subjects in different types of brain studies.\n"
 		"These can include functional MRI (fMRI), neurophysiology studies, EEG, magnetoencephalography, and diffusion tractography.");
-	Question bold = Question("In the acronym BOLD, the ‘B’ stands for....", 4, 3, "Bilirubin", "Bladder", "Blood", "Binding");
+	Question bold = Question("In the acronym BOLD, the â€˜Bâ€™ stands for....", 4, 3, "Bilirubin", "Bladder", "Blood", "Binding");
 	
 	Colors_q_I_made.run(&num_right);
 	brain_study.run(&num_right);
@@ -36,5 +36,5 @@ int main(int argc, char *argv[]) {
 	bold.run(&num_right);
 	printf("You got %d of %d questions right.\nThat's %d%%.\n", num_right, num_questions, (int)(100*(float)(num_right)/(float)num_questions));
 	if (num_right == num_questions) printf("Perfect score!\n");
-	
+	return 0;
 }
